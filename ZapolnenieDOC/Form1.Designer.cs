ff762bc8f105ad3ac9cb57eaaa55104f0dcbbb0e
Илья Лугановский <ilya.luganovskiy@mail.ru>
2020-03-31 02:45:36 +0300

    Фулл с уменьшенным интерфейсов 1200 х 800

--- ZapolnenieDOC/Form1.Designer.cs
+++ ZapolnenieDOC/Form1.Designer.cs
@@ -43,8 +43,8 @@
             this.progressBar2 = new System.Windows.Forms.ProgressBar();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.listBox2 = new System.Windows.Forms.ListBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
@@ -59,9 +59,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Rage Italic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(1413, 722);
+            this.button1.Location = new System.Drawing.Point(826, 631);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 88);
+            this.button1.Size = new System.Drawing.Size(201, 54);
             this.button1.TabIndex = 0;
             this.button1.Text = "START";
             this.button1.UseVisualStyleBackColor = true;
@@ -70,7 +70,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(687, 316);
+            this.label1.Location = new System.Drawing.Point(357, 213);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(383, 13);
             this.label1.TabIndex = 3;
@@ -79,16 +79,16 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(690, 333);
+            this.listBox1.Location = new System.Drawing.Point(360, 229);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(413, 225);
+            this.listBox1.Size = new System.Drawing.Size(419, 212);
             this.listBox1.TabIndex = 4;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(521, 216);
+            this.button3.Location = new System.Drawing.Point(785, 540);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(141, 54);
+            this.button3.Size = new System.Drawing.Size(125, 36);
             this.button3.TabIndex = 5;
             this.button3.Text = "Сохранить Результат Заполнения в ексель";
             this.button3.UseVisualStyleBackColor = true;
@@ -96,9 +96,9 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(521, 27);
+            this.button4.Location = new System.Drawing.Point(360, 12);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(141, 52);
+            this.button4.Size = new System.Drawing.Size(127, 42);
             this.button4.TabIndex = 6;
             this.button4.Text = "Поместить данные в таблицу сверху";
             this.button4.UseVisualStyleBackColor = true;
@@ -107,7 +107,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(702, 9);
+            this.label2.Location = new System.Drawing.Point(490, 18);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(164, 13);
             this.label2.TabIndex = 7;
@@ -115,7 +115,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(705, 28);
+            this.textBox1.Location = new System.Drawing.Point(493, 34);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(286, 20);
             this.textBox1.TabIndex = 8;
@@ -123,7 +123,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(705, 144);
+            this.textBox2.Location = new System.Drawing.Point(493, 103);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(286, 20);
             this.textBox2.TabIndex = 10;
@@ -132,7 +132,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(702, 127);
+            this.label4.Location = new System.Drawing.Point(490, 87);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(164, 13);
             this.label4.TabIndex = 11;
@@ -140,9 +140,9 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(521, 127);
+            this.button5.Location = new System.Drawing.Point(360, 81);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(141, 52);
+            this.button5.Size = new System.Drawing.Size(127, 42);
             this.button5.TabIndex = 13;
             this.button5.Text = "Поместить данные в таблицу снизу";
             this.button5.UseVisualStyleBackColor = true;
@@ -150,14 +150,14 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(705, 54);
+            this.progressBar1.Location = new System.Drawing.Point(493, 60);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(286, 15);
             this.progressBar1.TabIndex = 15;
             // 
             // progressBar2
             // 
-            this.progressBar2.Location = new System.Drawing.Point(705, 170);
+            this.progressBar2.Location = new System.Drawing.Point(493, 129);
             this.progressBar2.Name = "progressBar2";
             this.progressBar2.Size = new System.Drawing.Size(286, 15);
             this.progressBar2.TabIndex = 16;
@@ -165,7 +165,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(716, 217);
+            this.label3.Location = new System.Drawing.Point(904, 540);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(174, 13);
             this.label3.TabIndex = 17;
@@ -173,29 +173,29 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(705, 250);
+            this.textBox3.Location = new System.Drawing.Point(916, 556);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(286, 20);
+            this.textBox3.Size = new System.Drawing.Size(236, 20);
             this.textBox3.TabIndex = 18;
             this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
-            // 
-            // listBox2
-            // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(690, 599);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(413, 199);
-            this.listBox2.TabIndex = 20;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(687, 567);
+            this.label5.Location = new System.Drawing.Point(360, 444);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(312, 26);
             this.label5.TabIndex = 19;
             this.label5.Text = "Список студентов у которых были некоректные данные или\r\n их не было в таблице сни" +
     "зу";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(360, 473);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(419, 212);
+            this.listBox2.TabIndex = 20;
             // 
             // dataGridView1
             // 
@@ -203,32 +203,32 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(503, 385);
+            this.dataGridView1.Size = new System.Drawing.Size(342, 332);
             this.dataGridView1.TabIndex = 21;
             // 
             // dataGridView2
             // 
             this.dataGridView2.AllowUserToAddRows = false;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(12, 403);
+            this.dataGridView2.Location = new System.Drawing.Point(12, 353);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(503, 407);
+            this.dataGridView2.Size = new System.Drawing.Size(342, 332);
             this.dataGridView2.TabIndex = 22;
             // 
             // dataGridView3
             // 
             this.dataGridView3.AllowUserToAddRows = false;
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(1109, 98);
+            this.dataGridView3.Location = new System.Drawing.Point(785, 68);
             this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.Size = new System.Drawing.Size(483, 618);
+            this.dataGridView3.Size = new System.Drawing.Size(373, 466);
             this.dataGridView3.TabIndex = 23;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(1103, 54);
+            this.label6.Location = new System.Drawing.Point(838, 34);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(302, 31);
             this.label6.TabIndex = 24;
@@ -238,7 +238,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(997, 144);
+            this.label7.Location = new System.Drawing.Point(660, 87);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(100, 13);
             this.label7.TabIndex = 25;
@@ -247,7 +247,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(997, 27);
+            this.label8.Location = new System.Drawing.Point(663, 18);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(171, 13);
             this.label8.TabIndex = 26;
@@ -257,7 +257,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1605, 822);
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1169, 697);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -307,8 +309,8 @@
         private System.Windows.Forms.ProgressBar progressBar2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.DataGridView dataGridView3;
